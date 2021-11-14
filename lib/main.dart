e00405ec283d0fde3797528cf9b8d442a32d69d6
drunisa007 <newarunkatwal@gmail.com>
2021-11-15 00:15:17 +0630

    get x pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:whole_snack/core/utils/routes_pages.dart';
+import 'package:whole_snack/core/utils/theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Hive.initFlutter();
+  } else {
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    String appDocPath = appDocDir.path;
+    await Hive.initFlutter(appDocPath);
+  }
+  await Hive.openBox('authBox');
+  await Hive.openBox('apiBox');
+  await Hive.openBox('customerBox');
+
   runApp(const MyApp());
+
+/*  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ));*/
 }
 
 class MyApp extends StatelessWidget {
@@ -9,11 +36,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Whole Snack',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      builder: DevicePreview.appBuilder,
+      theme: StyleTheme().getTheme(),
+      getPages: RoutePage().routePage,
+      initialRoute: '/',
     );
   }
 }
