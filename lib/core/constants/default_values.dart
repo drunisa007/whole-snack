44644ea5e77657eb76c02d210226c2fc3062840f
drunisa007 <newarunkatwal@gmail.com>
2021-11-25 20:28:58 +0630

    font fix

--- lib/core/constants/default_values.dart
+++ lib/core/constants/default_values.dart
@@ -5,13 +5,18 @@ import 'package:flutter/material.dart';
  const String API = "https://seinwholesale.com/api/";
 
 /// font size for all app
-const double KAppBarTitleFontSize = 18.0;
+const double KAppBarTitleFontSize = 17.0;
 const double kExtraLargeFontSize16 = 16.0;
+const double kExtraLargeFontSize15 = 15.0;
 const double kLargeFontSize14 = 14.0;
+const double kLargeFontSize13 = 13.0;
 const double kMediumFontSize12 = 12.0;
+const double kMediumFontSize11 = 11.0;
 const double kSmallFontSize10 = 10.0;
+const double kSmallFontSize9 = 9.0;
 const double kExtraSmallFontSize8 = 8.0;
 const double kTinyFontSize7 = 7.0;
+const double kTinyFontSize6 = 7.0;
 const double kDefaultMargin = 12.0;
 
 
